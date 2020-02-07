0c15ab829c251a97378e1c6525826b8413fcf9b8
Jonathan McLatcher <jdmclatcher@gmail.com>
2020-02-07 13:18:38 -0500

    updated views, added filtering features

--- RFIDAttendance/Controllers/StudentsController.cs
+++ RFIDAttendance/Controllers/StudentsController.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -19,14 +18,82 @@ namespace RFIDAttendance.Controllers
             _context = context;
         }
 
+        // Button event handling
+        public IActionResult CheckIn()
+        {
+            long testID = 2000112143;
+            // code to run when button pressed
+            // TODO optimize this
+            var students = from s in _context.Student select s;
+            foreach(Student s in students)
+            {
+                if(s.StudentID == testID/*Convert.ToInt64(search)*/)
+                {
+                    // TODO: update of tardy, present, absent with bell schedule
+                    // PRESENT - if checking in by late bell
+                    // TARDY - will be changed from ABSENT to tardy when 
+                    // ABSENT - will be the default when late bell rings and havent checked in
+                    if (s.InClass)
+                    {
+                        // check out student
+                        s.TimeLastCheckedOut = DateTime.Now;
+                        s.InClass = false;
+                    } else
+                    {
+                        // check if has checked in before
+                        if (!s.TimeLastCheckedIn.HasValue)
+                        {
+                            // mark tardy if after late bell
+                            if((Convert.ToInt32(DateTime.Now.Hour) > 8) && (Convert.ToInt32(DateTime.Now.Minute) > 4))
+                            {
+                                s.AttendaceStatus = "TARDY";
+                            }
+                            else
+                            {
+                                s.AttendaceStatus = "PRESENT";
+                            }
+                        }
+                        // check in student
+                        s.TimeLastCheckedIn = DateTime.Now;
+                        s.InClass = true;   
+                    }
+                }
+            }
+            _context.SaveChanges(); // update Db
+            return RedirectToAction("Index");
+        }
+
+        // new day handling
+        public IActionResult NewDay()
+        {
+            var students = from s in _context.Student select s;
+            foreach(Student s in students)
+            {
+                s.AttendaceStatus = "ABSENT";
+                s.InClass = false;
+                s.TimeLastCheckedIn = null;
+                s.TimeLastCheckedOut = null;
+            }
+            _context.SaveChanges(); // update Db
+            return RedirectToAction("Index");
+        }
+        
         // GET: Students
-        public async Task<IActionResult> Index(string sortOrder)
+        public async Task<IActionResult> Index(string sortOrder, string search)
         {
             // sorting with hyperlinks
             ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
             ViewBag.IDSortParm = sortOrder == "ID" ? "ID_desc" : "ID";
+            ViewBag.StatusSortParm = sortOrder == "present" ? "present_desc" : "present";
             var students = from s in _context.Student
                            select s;
+            // search by name or student ID
+            if (!String.IsNullOrEmpty(search))
+            {
+                students = students.Where(s => s.Name.Contains(search)
+                                       || s.StudentID.ToString().Contains(search));
+            }
+            // sort based on input from Index
             switch (sortOrder)
             {
                 case "name_desc":
@@ -38,11 +105,17 @@ namespace RFIDAttendance.Controllers
                 case "ID_desc":
                     students = students.OrderByDescending(s => s.StudentID);
                     break;
+                case "present":
+                    students = students.OrderBy(s => s.AttendaceStatus);
+                    break;
+                case "present_desc":
+                    students = students.OrderByDescending(s => s.AttendaceStatus);
+                    break;
                 default:
                     students = students.OrderBy(s => s.Name);
                     break;
             }
-            return View(/*await _context.Student.ToListAsync()*/ await students.ToListAsync());
+            return View(await students.ToListAsync());
         }
 
         // GET: Students/Details/5
--- RFIDAttendance/Models/Student.cs
+++ RFIDAttendance/Models/Student.cs
@@ -10,21 +10,24 @@ namespace RFIDAttendance.Models
     {
         // unique ID needed for database
         public int Id { get; set; }
+        
         [Required(ErrorMessage = "A Student ID is required"), Range(1000000000, 4000000000, ErrorMessage = "Please enter a valid Student ID")]
         public long StudentID { get; set; }
+        
         [Required(ErrorMessage = "A {0} is required"), StringLength(100, MinimumLength = 1)]
         public string Name { get; set; }
         // status that marks if currently in class or not
+
         [Display(Name = "In Class?")]
-        public bool InClass { get; set; }
+        public bool InClass { get; set; } = false;
+        
         [DataType(DataType.Time), Display(Name = "Last Checked In")]
-        public string TimeLastCheckedIn { get; set; }
+        public DateTime? TimeLastCheckedIn { get; set; }
+        
         [DataType(DataType.Time), Display(Name = "Last Checked Out")]
-        public string TimeLastCheckedOut { get; set; }
-        // PRESENT - if checking in by late bell
-        // TARDY - will be changed from ABSENT to tardy when 
-        // ABSENT - will be the default when late bell rings and havent checked in
+        public DateTime? TimeLastCheckedOut { get; set; }
+
         [Display(Name = "Attendance Status")]
-        public string AttendaceStatus { get; set; }
+        public string AttendaceStatus { get; set; } = "ABSENT";
     }
 }
