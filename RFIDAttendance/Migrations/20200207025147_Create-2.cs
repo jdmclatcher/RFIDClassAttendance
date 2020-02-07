4362f56c141d014659e46457e6cb81bc3e070505
Jonathan McLatcher <jdmclatcher@gmail.com>
2020-02-07 13:18:09 -0500

    updated DB model

---
+++ RFIDAttendance/Migrations/20200207025147_Create-2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RFIDAttendance.Migrations
+{
+    public partial class Create2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "TimeLastCheckedOut",
+                table: "Student",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "TimeLastCheckedIn",
+                table: "Student",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "TimeLastCheckedOut",
+                table: "Student",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "TimeLastCheckedIn",
+                table: "Student",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldNullable: true);
+        }
+    }
+}
